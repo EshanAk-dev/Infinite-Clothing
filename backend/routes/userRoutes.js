98cc7eb8b4c6d68d45a08dd63b7fab42402c20ed
EshanAk-dev <eshanwijesinghe01@gmail.com>
2025-04-29 13:35:37 +0530

    fix: Extend JWT expiration time to 7 days for user registration and login

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -27,7 +27,7 @@ router.post("/register", async (req, res) => {
     jwt.sign(
       payload,
       process.env.JWT_SECRET,
-      { expiresIn: "2h" },
+      { expiresIn: "7d" },
       (err, token) => {
         if (err) throw err;
 
@@ -72,7 +72,7 @@ router.post("/login", async (req, res) => {
     jwt.sign(
       payload,
       process.env.JWT_SECRET,
-      { expiresIn: "2h" },
+      { expiresIn: "7d" },
       (err, token) => {
         if (err) throw err;
 
